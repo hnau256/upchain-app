78c70c0358f4fee1d861f2ca2fca0cb50fe3896e
Mark Zorikhin <hnau256@gmail.com>
2026-04-01 13:55:27 +0300

    Add port and TLS command-line options to server and client
    - Add optional `--port` (`-p`) argument to the server to configure the listening port.
    - Add optional `--port` (`-p`) and `--tls` flags to the client to configure connection settings.
    - Update `HttpSyncClient` and `httpSyncServer` initialization to use the new CLI arguments.
    - Reformat CLI option declarations in both `Main.kt` files for better readability.

--- client/src/main/kotlin/org/hnau/upchainapp/client/Main.kt
+++ client/src/main/kotlin/org/hnau/upchainapp/client/Main.kt
@@ -4,10 +4,13 @@ import co.touchlab.kermit.Logger
 import co.touchlab.kermit.platformLogWriter
 import kotlinx.cli.ArgParser
 import kotlinx.cli.ArgType
+import kotlinx.cli.default
 import kotlinx.cli.required
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.runBlocking
 import org.hnau.commons.kotlin.coroutines.createChild
+import org.hnau.commons.kotlin.foldBoolean
+import org.hnau.commons.kotlin.foldNullable
 import org.hnau.upchain.core.UpchainId
 import org.hnau.upchain.core.repository.file.upchain.fileBased
 import org.hnau.upchain.core.repository.upchain.UpchainRepository
@@ -19,42 +22,76 @@ import org.hnau.upchain.sync.http.HttpScheme
 
 private val logger = Logger.withTag("Main")
 
-fun main(
-    args: Array<String>,
-) {
+fun main(args: Array<String>) {
     Logger.setLogWriters(platformLogWriter())
 
     val parser = ArgParser("Upchain client")
-    val upchainsFile by parser.option(
-        type = ArgType.String,
-        shortName = "f",
-    ).required()
-    val serverAddress by parser.option(
-        type = ArgType.String,
-        shortName = "s",
-    ).required()
+
+    val upchainsFile by parser
+        .option(
+            type = ArgType.String,
+            shortName = "f",
+        )
+        .required()
+
+    val serverAddress by parser
+        .option(
+            type = ArgType.String,
+            shortName = "s",
+        )
+        .required()
+
+    val port by parser.option(
+        type = ArgType.Int,
+        shortName = "p",
+    )
+
+    val useTls by parser
+        .option(
+            type = ArgType.Boolean,
+            shortName = "tls",
+        )
+        .default(false)
+
     parser.parse(args)
 
     runBlocking {
-
-        val repository = UpchainRepository.fileBased(
-            filename = upchainsFile,
-        )
+        val repository =
+            UpchainRepository.fileBased(
+                filename = upchainsFile,
+            )
 
         val clientScope = createChild()
-        val api = HttpSyncClient(
-            scope = clientScope,
-            address = ServerAddress(serverAddress),
-            port = ServerPort(8080),
-            scheme = HttpScheme.Http,
+        val address = ServerAddress(serverAddress)
+        val scheme = useTls.foldBoolean(
+            ifTrue = { HttpScheme.Https },
+            ifFalse = { HttpScheme.Http },
         )
+        val api = port
+            ?.let(::ServerPort)
+            .foldNullable(
+                ifNull = {
+                    HttpSyncClient(
+                        scope = clientScope,
+                        address = address,
+                        scheme = scheme,
+                    )
+                },
+                ifNotNull = { port ->
+                    HttpSyncClient(
+                        scope = clientScope,
+                        address = address,
+                        port = port,
+                        scheme = scheme,
+                    )
+                }
+            )
 
         repository
             .sync(
                 id = upchainsFile.split("/").last().let(UpchainId.stringMapper.direct),
                 api = api,
-            )
-            .getOrThrow()
+            ).getOrThrow()
 
         logger.i { "Synchronized" }
 
--- server/src/main/kotlin/org/hnau/upchainapp/server/Main.kt
+++ server/src/main/kotlin/org/hnau/upchainapp/server/Main.kt
@@ -6,6 +6,7 @@ import kotlinx.cli.ArgParser
 import kotlinx.cli.ArgType
 import kotlinx.cli.required
 import kotlinx.coroutines.runBlocking
+import org.hnau.commons.kotlin.foldNullable
 import org.hnau.upchain.core.repository.file.upchains.fileBased
 import org.hnau.upchain.core.repository.upchains.UpchainsRepository
 import org.hnau.upchain.sync.core.ServerPort
@@ -13,18 +14,23 @@ import org.hnau.upchain.sync.server.core.ServerEngine
 import org.hnau.upchain.sync.server.core.repository.toCreateOnly
 import org.hnau.upchain.sync.server.http.httpSyncServer
 
-fun main(
-    args: Array<String>,
-) {
+fun main(args: Array<String>) {
     Logger.setLogWriters(platformLogWriter())
 
     val parser = ArgParser("Upchain server")
-    val upchainsDir by parser.option(
-        type = ArgType.String,
-        shortName = "d",
-    ).required()
-    parser.parse(args)
 
+    val upchainsDir by parser
+        .option(
+            type = ArgType.String,
+            shortName = "d",
+        )
+        .required()
+
+    val port by parser.option(
+        type = ArgType.Int,
+        shortName = "p",
+    )
+    parser.parse(args)
 
     runBlocking {
 
@@ -37,9 +43,21 @@ fun main(
             repository = repository.toCreateOnly(this),
         )
 
-        httpSyncServer(
-            engine = engine,
-            port = ServerPort(8080),
-        )
+        port
+            ?.let(::ServerPort)
+            .foldNullable(
+                ifNull = {
+                    httpSyncServer(
+                        engine = engine,
+                    )
+                },
+                ifNotNull = { port ->
+                    httpSyncServer(
+                        engine = engine,
+                        port = port,
+                    )
+                }
+            )
+
     }
 }
